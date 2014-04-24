aaa4127cc83d83a90678f9985f08c3e00961fb63
Aaron Bedra <aaron@aaronbedra.com>
2014-04-24 16:14:16 -0500

    Wiring up record function
    This sets up the initial wiring for recording requests. Cleanup of the
    way timestamps get produced is necessary.
    
    Signed-off-by: Aaron Bedra <aaron@aaronbedra.com>

--- spec/integration/integration_spec.rb
+++ spec/integration/integration_spec.rb
@@ -18,6 +18,31 @@ describe "Integration Specs" do
     end
   end
 
+  describe "Recorder" do
+    it "Records the IP, User Agent, Method, URI, and Arguments during a request" do
+      Curl.get "http://127.0.0.1:8888"
+
+      @redis.llen("127.0.0.1:requests").should == 1
+    end
+
+    it "Properly sets the expiry" do
+      Curl.get "http://127.0.0.1:8888"
+
+      @redis.ttl("127.0.0.1:requests").should > 1
+    end
+
+    it "Records activity using the proper IP when behind a proxy" do
+      pending("waiting for XFF processing to be implemented") do
+        http = Curl.get("http://127.0.0.1:8888") do |http|
+          http.headers['X-Forwarded-For'] = '1.1.1.1'
+        end
+
+        @redis.llen("1.1.1.1:requests").should == 1
+      end
+    end
+
+  end
+
   describe "Actions" do
     it "Returns a 403 response if the actor is on the blacklist" do
       @redis.set("127.0.0.1:repsheet:blacklist", "true")
--- src/mod_repsheet.c
+++ src/mod_repsheet.c
@@ -10,6 +10,8 @@ typedef struct {
   const char *redis_host;
   int redis_port;
   int redis_timeout;
+  int redis_expiry;
+  int redis_max_length;
 
 } repsheet_config;
 
@@ -58,12 +60,32 @@ const char *repsheet_set_port(cmd_parms *cmd, void *cfg, const char *arg)
   }
 }
 
+const char *repsheet_set_redis_max_length(cmd_parms *cmd, void *cfg, const char *arg)
+{
+  int length = atoi(arg);
+
+  if (length > 0) {
+    config.redis_max_length = length;
+    return NULL;
+  } else {
+    return "[ModRepsheet] - The RepsheetRedisMaxLength directive must be a number";
+  }
+}
+
+const char *repsheet_set_redis_expiry(cmd_parms *cmd, void *cfg, const char *arg)
+{
+  config.redis_expiry = atoi(arg) * 60 * 60;
+  return NULL;
+}
+
 static const command_rec repsheet_directives[] =
   {
-    AP_INIT_TAKE1("repsheetEnabled",      repsheet_set_enabled, NULL, RSRC_CONF, "Enable or disable mod_repsheet"),
-    AP_INIT_TAKE1("repsheetRedisTimeout", repsheet_set_timeout, NULL, RSRC_CONF, "Set the Redis timeout"),
-    AP_INIT_TAKE1("repsheetRedisHost",    repsheet_set_host,    NULL, RSRC_CONF, "Set the Redis host"),
-    AP_INIT_TAKE1("repsheetRedisPort",    repsheet_set_port,    NULL, RSRC_CONF, "Set the Redis port"),
+    AP_INIT_TAKE1("repsheetEnabled",        repsheet_set_enabled,          NULL, RSRC_CONF, "Enable or disable mod_repsheet"),
+    AP_INIT_TAKE1("repsheetRedisTimeout",   repsheet_set_timeout,          NULL, RSRC_CONF, "Set the Redis timeout"),
+    AP_INIT_TAKE1("repsheetRedisHost",      repsheet_set_host,             NULL, RSRC_CONF, "Set the Redis host"),
+    AP_INIT_TAKE1("repsheetRedisPort",      repsheet_set_port,             NULL, RSRC_CONF, "Set the Redis port"),
+    AP_INIT_TAKE1("repsheetRedisMaxLength", repsheet_set_redis_max_length, NULL, RSRC_CONF, "Last n requests kept per IP"),
+    AP_INIT_TAKE1("repsheetRedisExpiry",    repsheet_set_redis_expiry,     NULL, RSRC_CONF, "Number of hours before records expire"),
     { NULL }
   };
 
@@ -86,16 +108,27 @@ static int act(request_rec *r)
 #endif
 
   if (is_whitelisted(context, actor_address)) {
+    ap_log_error(APLOG_MARK, APLOG_ERR, 0, r->server, "%s is whitelisted by repsheet", actor_address);
     redisFree(context);
     return DECLINED;
   } else if (is_blacklisted(context, actor_address)) {
-    ap_log_error(APLOG_MARK, APLOG_ERR, 0, r->server, "%s was blocked by the repsheet", actor_address);
+    ap_log_error(APLOG_MARK, APLOG_ERR, 0, r->server, "%s was blocked by repsheet", actor_address);
     redisFree(context);
     return HTTP_FORBIDDEN;
   } else if (is_on_repsheet(context, actor_address)) {
-    ap_log_error(APLOG_MARK, APLOG_ERR, 0, r->server, "IP Address %s was found on the repsheet. No action taken", actor_address);
+    ap_log_error(APLOG_MARK, APLOG_ERR, 0, r->server, "%s was found on repsheet. No action taken", actor_address);
     apr_table_set(r->headers_in, "X-Repsheet", "true");
   }
+
+  apr_time_exp_t start;
+  char timestamp[50];
+
+  apr_time_exp_gmt(&start, r->request_time);
+  sprintf(timestamp, "%d/%d/%d %d:%d:%d.%d", (start.tm_mon + 1), start.tm_mday, (1900 + start.tm_year), start.tm_hour, start.tm_min, start.tm_sec, start.tm_usec);
+
+  record(context, timestamp, apr_table_get(r->headers_in, "User-Agent"),
+	 r->method, r->uri, r->args, config.redis_max_length, config.redis_expiry,
+	 actor_address);
 
   redisFree(context);
 
